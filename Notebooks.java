432051b1ab5af506d341d343664b90b36172c1cd
Aleksandr-Anokhin <aleksandr0788@gmail.com>
2023-02-09 16:38:19 +0300

    made changes to class Notebooks

--- Notebooks.java
+++ Notebooks.java
@@ -5,13 +5,15 @@ public class Notebooks {
     private String hardDisk;
     private String OS;
     private String colour;
+    private String Price;
     
-    public Notebooks(String brandName, String RAM, String hardDisk, String OS, String colour) {
+    public Notebooks(String brandName, String RAM, String hardDisk, String OS, String colour, String Price) {
         this.brandName = brandName;
         this.RAM = RAM;
         this.hardDisk = hardDisk;
         this.OS = OS;
         this.colour = colour;
+        this.Price = Price;
     }
 
     public String getName() {
@@ -36,9 +38,13 @@ public class Notebooks {
         return colour;
     }
 
+    public String getPrice() {
+        return Price;
+    }
+
     @Override
     public String toString() {
         return "Notebooks [name = " + brandName + ", ram = " + RAM + ", hardDisk = " + hardDisk + ", operatingSystem = "
-                + OS + ", colour = " + colour + "]";
+                + OS + ", colour = " + colour + ", Price = " + Price + "]";
     }
 }
