99526cf421010781cac519d479ee9f6d36301cd3
Aleksandr-Anokhin <aleksandr0788@gmail.com>
2023-02-10 18:46:55 +0300

    to be continued

--- task6_1.java
+++ task6_1.java
@@ -3,6 +3,7 @@
  Критерии фильтрации можно хранить в Map. 
 */
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -20,8 +21,8 @@ public class task6_1 {
         Notebooks notebook3 = new Notebooks("Lenovo", "12", "512 Gb", "Win 10", "Dark blue", "45000 руб.");
         Notebooks notebook4 = new Notebooks("MSI", "16", "512 Gb", "Linux", "Black", "43000 руб.");
         
-        Set<Notebooks> notebooks = new HashSet<>(Notebooks.asList(notebook1, notebook2, notebook3, notebook4));
-        // Ошибка(подчеркнуто всё после знака =): Cannot infer type arguments for HashSet<>Java(16778094)
+        Set<Notebooks> notebooks = new HashSet<>(Arrays.asList(notebook1, notebook2, notebook3, notebook4));
+        // 
 
         Map<String, String> sel = selectCriteria();
         sort(sel, notebooks);    
@@ -37,11 +38,15 @@ public class task6_1 {
      public static Map<String, String> selectCriteria() {
         Map<String, String> resultCriterias = new HashMap<>();     
         
-        System.out.println("Выберите номер критерия: \n 1 - Название производителя \n 2 - Объем ОЗУ \n 3 - Объем Жесткого Диска \n 4 - Операционная система \n 5 - Цвет \n 6 - Цена");
-        String key = scanner();
+        System.out.println("Выберите номер критерия: \n 1 - Объем ОЗУ \n 2 - Объем Жесткого Диска \n 3 - Операционная система \n 4 - Цвет \n 5 - Цена");
+        //String key = scanner();
+
+        System.out.println("Введите значения для выбранного критерия: ");
+        String value = scanner();
+
         
-        resultCriterias.put(key);
-        // Ошибка для put: The method put(String, String) in the type Map<String,String> is not applicable for the arguments (String)Java(67108979)
+        resultCriterias.put("1", value);
+        // 
 
         return resultCriterias;
     }
@@ -50,42 +55,27 @@ public class task6_1 {
 
         Set<Notebooks> temp = new HashSet<>(notebooks);
         for (Notebooks notebook : notebooks) {          
-            for (Object pair1 : criterias.keySet()) {          
-                if (pair1.equals("1") && !notebook.getName().equals(criterias.get(pair1))) {
-                    temp.remove(notebook);
-                }
-                for (Object pair2 : criterias.keySet()) {          
-                    if (pair2.equals("2") && !notebook.getRam().equals(criterias.get(pair2))) {
-                        temp.remove(notebook);          
-                    }
-                    for (Object pair3 : criterias.keySet()) {          
-                        if (pair3.equals("3") && !notebook.getHardDisk().equals(criterias.get(pair3))) {
-                               temp.remove(notebook);          
-                        }
-                        for (Object pair4 : criterias.keySet()) {          
-                            if (pair4.equals("4") && !notebook.getOperatingSystem().equals(criterias.get(pair4))) {
-                                temp.remove(notebook);          
-                            }
-                            for (Object pair5 : criterias.keySet()) {          
-                                if (pair5.equals("5") && !notebook.getColour().equals(criterias.get(pair5))) {
-                                    temp.remove(notebook);
-          
-                                }
-                                for (Object pair6 : criterias.keySet()) {          
-                                    if (pair6.equals("6") && !notebook.getPrice().equals(criterias.get(pair6))) {
-                                        temp.remove(notebook);
-              
-                                    }
-                                }
-                            }
-                        }
-                    }
+            for (Object pair2 : criterias.keySet()) {          
+                if (pair2.equals("2")) {
+                    temp.put(notebook.getName(), notebook.getRam());
+                } 
+            }
+            for (Object pair3 : criterias.keySet()) { 
+                if (pair3.equals("3")) {
+                    temp.put(notebook.getName(), notebook.getHardDisk());          
                 }
             }
-          
         }
+        /*  Подчеркнуто put. Ошибки такие: Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
+        The method put(String, String) is undefined for the type Set<Notebooks>
+        The method put(String, String) is undefined for the type Set<Notebooks>
+
+        at task6_1.sort(task6_1.java:60)
+        at task6_1.main(task6_1.java:28)
+         */ 
+        
         if (temp.isEmpty()) {
-            System.out.println("По введенным критериям ничего не найдено!");
+            System.out.println("Некорректные данные, попробуйте ещё раз. ");
             } else {
                 System.out.println("Вот что мы можем предложить: \n" + temp.toString());
             }
