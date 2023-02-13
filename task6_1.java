f80c7b5a32ee9ca92f60087d09ee54a4e892d8c7
Aleksandr-Anokhin <aleksandr0788@gmail.com>
2023-02-13 22:05:27 +0300

    redid the final version

--- task6_1.java
+++ task6_1.java
@@ -18,10 +18,10 @@ public class task6_1 {
      * @param args
      */
     public static void main(String[] args) {
-        Notebooks notebook1 = new Notebooks("ACER", "8", "256 Gb", "Win 10", "Silver", "25000 руб.");
-        Notebooks notebook2 = new Notebooks("ASUS", "12", "256 Gb", "No OS", "Grey", "30000 руб.");
+        Notebooks notebook1 = new Notebooks("ACER  ", "8 ", "256 Gb", "Win 10", "Silver   ", "25000 руб.");
+        Notebooks notebook2 = new Notebooks("ASUS  ", "12", "256 Gb", "No OS ", "Grey     ", "30000 руб.");
         Notebooks notebook3 = new Notebooks("Lenovo", "12", "512 Gb", "Win 10", "Dark blue", "45000 руб.");
-        Notebooks notebook4 = new Notebooks("MSI", "16", "512 Gb", "Linux", "Black", "43000 руб.");
+        Notebooks notebook4 = new Notebooks("MSI   ", "16", "512 Gb", "Linux ", "Black    ", "43000 руб.");
         
         Set<Notebooks> notebooks = new HashSet<>(Arrays.asList(notebook1, notebook2, notebook3, notebook4));
         // 
@@ -42,17 +42,14 @@ public class task6_1 {
         
         System.out.println("Выберите номер критерия: \n 1 - Объем ОЗУ \n 2 - Объем Жесткого Диска \n 3 - Операционная система \n 4 - Цвет \n 5 - Цена");
        
-        String value = scanner();
-
-        
-        resultCriterias.put("1", value);
-        // 
+        String value = scanner();        
+        resultCriterias.put("1", value);     
 
         return resultCriterias;
     }
     public static void ShowMapLaptops(Map<String, Object> map, String param) {
         Map<String,Object> sortedMap = new TreeMap<>(map);
-        for (Map.Entry<String, Object> el : sortedMap.entrySet()) {
+        for (Map.Entry<String, Object> el : sortedMap.entrySet()) {            
             System.out.println("| " + el.getKey() + " | " + el.getValue() + param + " | ");
         }
     }
@@ -64,17 +61,65 @@ public class task6_1 {
         ShowMapLaptops(lapRam, " ГБ");//вывод красивый
         return lapRam;
     }
-
-    public static void sort(Map<String, String> criterias, Set<Notebooks> notebooks) {
-
-       
-        if (criterias.values().contains("1"))
-            getAllRAM(notebooks);
-        else if (criterias.values().contains("2"))
-            getAllRAM(notebooks);
-                 
+    public static Map<String, Object> getAllHDD(Set<Notebooks> list) {
+        Map<String, Object> lapRam = new HashMap<>();
+        for (Notebooks el : list) {
+            lapRam.put(el.getName(), el.getHardDisk()); 
+        }
+        ShowMapLaptops(lapRam, " ");
+        return lapRam;
+    }
+    public static Map<String, Object> getAll_OS(Set<Notebooks> list) {
+        Map<String, Object> lapRam = new HashMap<>();
+        for (Notebooks el : list) {
+            lapRam.put(el.getName(), el.getOperatingSystem()); 
+        }
+        ShowMapLaptops(lapRam, " ");
+        return lapRam;
+    }
+    public static Map<String, Object> getAllColour(Set<Notebooks> list) {
+        Map<String, Object> lapRam = new HashMap<>();
+        for (Notebooks el : list) {
+            lapRam.put(el.getName(), el.getColour()); 
+        }
+        ShowMapLaptops(lapRam, " ");
+        return lapRam;
+    }
+    public static Map<String, Object> getAllPrice(Set<Notebooks> list) {
+        Map<String, Object> lapRam = new HashMap<>();
+        for (Notebooks el : list) {
+            lapRam.put(el.getName(), el.getPrice()); 
+        }
+        ShowMapLaptops(lapRam, " ");
+        return lapRam;
     }
 
-    
+    public static void sort(Map<String, String> criterias, Set<Notebooks> notebooks) {       
+        if (criterias.values().contains("1"))
+            getAllRAM(notebooks);
+        else if (criterias.values().contains("1"))
+            getAllRAM(notebooks);
+
+        if (criterias.values().contains("2"))
+            getAllHDD(notebooks);
+        else if (criterias.values().contains("2"))
+            getAllHDD(notebooks);
+
+        if (criterias.values().contains("3"))
+            getAll_OS(notebooks);
+        else if (criterias.values().contains("3"))
+            getAll_OS(notebooks);
+
+        if (criterias.values().contains("4"))
+            getAllColour(notebooks);
+        else if (criterias.values().contains("4"))
+            getAllColour(notebooks);
+
+        if (criterias.values().contains("5"))
+            getAllPrice(notebooks);
+        else if (criterias.values().contains("5"))
+            getAllPrice(notebooks);
+                 
+    }    
 }
   
